029085f1b0dc10e8151d3ae4152b25ec3386fdea
morj <xopco@yandex.ru>
2021-10-25 19:45:17 +0500

    1

---
+++ config1.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.89.0"
+    }
+  }
+}
+
+provider "google" {
+ credentials = file("CREDENTIALS_FILE.json")
+ project     = "new1-330022"
+ region      = "europe-north1"
+}
+
+// Terraform plugin for creating random ids
+resource "random_id" "instance_id" {
+ byte_length = 1
+}
+
+// A single Compute Engine instance
+resource "google_compute_instance" "default" {
+  name = "vm-${random_id.instance_id.hex}"
+  machine_type = "f1-micro"
+  zone = "europe-north1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-2004-lts"
+    }
+  }
+  metadata_startup_script = "sudo apt-get -y update; sudo apt-get -y install mc"
+
+ network_interface {
+   network = "default"
+  }
+}
